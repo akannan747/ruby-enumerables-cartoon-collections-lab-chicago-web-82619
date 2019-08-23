79430fadb12633d2b9d09fdc6ca162ecb59297a3
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 17:53:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(calls)# code an argument here
 end
 
 def long_planeteer_calls(calls)# code an argument here
-  calls.select { |call| call.length > 4) }.length > 0
+  calls.select { |call| call.length > 4 }.length > 0
 end
 
 def find_the_cheese# code an argument here
