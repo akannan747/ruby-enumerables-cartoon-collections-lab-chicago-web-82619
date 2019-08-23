74a153cc32836f47ca3403c8ffffbc57b65069f3
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 17:24:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index {|item, index| 
+    puts "#{index+1}. #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)# code an argument here
+  calls.map {|word| word.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
